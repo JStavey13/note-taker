da9f2c577f66351309207bdc32abb3f404e48674
Jartius <JStavey13@users.noreply.github.com>
2023-02-01 21:21:19 -0500

    added server.js file, and began setting it up

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
+const PORT = 3001
+
+const app = express()
+
+
+
+
+
+
+
+
+
+
+// server
+app.listen(PORT, () =>
+console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
